d502ffe08f1866b012cc3044af8f3c15164988cd
Raycoms <ray.neiheiser@gmail.com>
2024-08-22 18:53:13 +0200

    small adjustment

--- src/main/java/com/ldtteam/towntalk/TownTalk.java
+++ src/main/java/com/ldtteam/towntalk/TownTalk.java
@@ -3,23 +3,16 @@ package com.ldtteam.towntalk;
 import com.ldtteam.towntalk.generation.DefaultSoundProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackLocationInfo;
-import net.minecraft.server.packs.PackSelectionConfig;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.PathPackResources;
-import net.minecraft.server.packs.repository.BuiltInPackSource;
-import net.minecraft.server.packs.repository.KnownPack;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackSource;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.javafmlmod.FMLModContainer;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.event.AddPackFindersEvent;
-
-import java.util.Optional;
 
 @Mod("towntalk")
 public class TownTalk
@@ -40,15 +33,6 @@ public class TownTalk
 
     public void addResourcePack(final AddPackFindersEvent event)
     {
-        if (event.getPackType() == PackType.CLIENT_RESOURCES)
-        {
-            var resourcePath = ModList.get().getModFileById("towntalk").getFile().findResource("respack");
-
-            var pack = Pack.readMetaAndCreate(
-              new PackLocationInfo("mod/builtin/towntalk", Component.literal("Town Talk"), PackSource.BUILT_IN, Optional.of(new KnownPack("towntalk", "mod/builtin/towntalk", "1.0"))),
-              BuiltInPackSource.fromName((path) -> new PathPackResources(path, resourcePath)),
-              PackType.CLIENT_RESOURCES,
-              new PackSelectionConfig(true, Pack.Position.BOTTOM, false));
-        }
+        event.addPackFinders(ResourceLocation.fromNamespaceAndPath("towntalk", "respack"), PackType.CLIENT_RESOURCES, Component.literal("Town Talk"), PackSource.BUILT_IN, true, Pack.Position.TOP);
     }
 }
